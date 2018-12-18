be3f681ddf0903831d24372c4076d30f6f0647b8
吴炎冲 <363648823@qq.com>
2018-12-18 09:05:18 +0800

    配置文件修改为测试环境地址

--- config/index.js
+++ config/index.js
@@ -12,11 +12,8 @@ module.exports = {
        * 开发时调用不同服务端API时，需添加排除 !** /register
        */
       context: ['/crmWebApi/**/*'],
-      // target: 'http://127.0.0.1:8089',
       // 正式 target: 'http://120.27.159.74:80',
-      // target: 'http://47.96.228.119:8089',
-      target: 'http://120.27.159.74:80',
-      // target: 'http://127.0.0.1:30001',
+      target: 'http://47.96.228.119:8089',
       // target: 'http://localhost:30001',
       changeOrigin: true,
       // secure: false,  // 如果是https接口，需要配置这个参数
@@ -31,14 +28,13 @@ module.exports = {
       context: ['/casLogin'],
       // target: "http://120.27.159.167:30001", // local: 'http://localhost:8360', formal:  'http://120.27.159.167:30001'
       // 正式 target: 'http://120.27.159.74:80',
-      // target: 'http://47.96.228.119:8089',
-      target: 'http://120.27.159.74:80',
+      target: 'http://47.96.228.119:8089',
       changeOrigin: true,
     }
     ],
     // Various Dev Server settings
     host: 'localhost', // can be overwritten by process.env.HOST
-    port: 80, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
+    port: 8080, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: false,
     errorOverlay: true,
     notifyOnErrors: true,
