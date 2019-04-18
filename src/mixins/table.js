9e5644be33fa1a277d1e21c16c23a4faa940b2ba
魏超超 <753173952@qq.com>
2019-04-18 14:29:49 +0800

    去除无用代码

--- src/mixins/table.js
+++ src/mixins/table.js
@@ -155,7 +155,6 @@ export default {
     },
     // 重置功能
     $resetInputAction$: function () {
-      console.log(111)
       if (typeof this.$resetInput === 'function') {
         var model = this.$resetInput(this.model)
         if (model) {
