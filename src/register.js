69ddc8f537ade6ad9d3dafd282a58b573191988c
曾建福 <jinafu.zeng@nascent.cn>
2019-04-23 17:09:06 +0800

    up directives filters

--- src/register.js
+++ src/register.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
-import directives from './directives'
+import directives from 'web-crm/src/directives'
+import directivesPrivate from './directives'
 import components from './components/global'
 import mixins from './mixins/global'
 import ets from './extends'
-import filters from './filters'
+import filters from 'web-crm/src/filters'
+import filtersPrivate from './filters'
 import numeral from 'numeral'
 
 Vue.prototype.$numeral = numeral
@@ -16,6 +18,7 @@ Vue.use(ets)
  * 注册全局指令
  */
 Vue.use(directives)
+Vue.use(directivesPrivate)
 
 /**
  * 注册全局组件
@@ -31,3 +34,4 @@ Vue.use(mixins)
  * 全局注册过滤器
  */
 filters.register(Vue)
+filtersPrivate.register(Vue)
