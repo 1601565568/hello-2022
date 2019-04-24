e2eaaf8098eff403874858a481f3e0ef26176887
涂海波 <1147104744@qq.com>
2019-04-24 18:35:19 +0800

    修改common.js

--- src/components/common.js
+++ src/components/common.js
@@ -1,2 +1,175 @@
+// import Alert from 'nui-v2/lib/alert'
+// import Aside from 'nui-v2/lib/aside'
+// import Autocomplete from 'nui-v2/lib/autocomplete'
+// import Badge from 'nui-v2/lib/badge'
+// import Breadcrumb from 'nui-v2/lib/breadcrumb'
+// import BreadcrumbItem from 'nui-v2/lib/breadcrumb-item'
+import ButtonGroup from 'nui-v2/lib/button-group'
+// import Calendar from 'nui-v2/lib/calendar'
+// import Card from 'nui-v2/lib/card'
+// import Carousel from 'nui-v2/lib/carousel'
+// import CarouselItem from 'nui-v2/lib/carousel-item'
+// import Cascader from 'nui-v2/lib/cascader'
+import Checkbox from 'nui-v2/lib/checkbox'
+// import CheckboxButton from 'nui-v2/lib/checkbox-button'
+import CheckboxGroup from 'nui-v2/lib/checkbox-group'
+import Col from 'nui-v2/lib/col'
+import Collapse from 'nui-v2/lib/collapse'
+import CollapseItem from 'nui-v2/lib/collapse-item'
+// import ColorPicker from 'nui-v2/lib/color-picker'
+// import Container from 'nui-v2/lib/container'
+// import Countup from 'nui-v2/lib/countup'
+import DatePicker from 'nui-v2/lib/date-picker'
+import Dialog from 'nui-v2/lib/dialog'
+import Dropdown from 'nui-v2/lib/dropdown'
+import DropdownItem from 'nui-v2/lib/dropdown-item'
+import DropdownMenu from 'nui-v2/lib/dropdown-menu'
+// import Footer from 'nui-v2/lib/footer'
+import Form from 'nui-v2/lib/form'
+import FormGrid from 'nui-v2/lib/form-grid'
+import FormItem from 'nui-v2/lib/form-item'
+// import Header from 'nui-v2/lib/header'
+// import Icon from 'nui-v2/lib/icon'
+import Input from 'nui-v2/lib/input'
+// import InputNumber from 'nui-v2/lib/input-number'
+import Loading from 'nui-v2/lib/loading'
+// import Main from 'nui-v2/lib/main'
+// import Menu from 'nui-v2/lib/menu'
+// import MenuItem from 'nui-v2/lib/menu-item'
+// import MenuItemGroup from 'nui-v2/lib/menu-item-group'
+import Message from 'nui-v2/lib/message'
+import MessageBox from 'nui-v2/lib/message-box'
+// import Multi from 'nui-v2/lib/multi'
+import Notification from 'nui-v2/lib/notification'
+
+// 自定义组件
+import NsButton from './NsButton'
+import NsCountDown from './NsCountDown'
+import NsPageCharts from './NsPageCharts'
+import NsPageTable from './NsPageTable'
+import NsNoData from './NsNoData'
+import NsSave from './NsSave'
+import NsSelect from './NsSelect'
+import NsTableColumnOperateButton from './NsTableColumnOperateButton'
+import NsTableOperateButton from './NsTableOperateButton'
+// 自定义组件结束
+
+import Option from 'nui-v2/lib/option'
+import OptionGroup from 'nui-v2/lib/option-group'
+import Pagination from 'nui-v2/lib/pagination'
+import Popover from 'nui-v2/lib/popover'
+// import Progress from 'nui-v2/lib/progress'
+import Radio from 'nui-v2/lib/radio'
+import RadioButton from 'nui-v2/lib/radio-button'
+import RadioGroup from 'nui-v2/lib/radio-group'
+// import Rate from 'nui-v2/lib/rate'
+// import RegionSelection from 'nui-v2/lib/region-selection'
+import Row from 'nui-v2/lib/row'
+import Scrollbar from 'nui-v2/lib/scrollbar'
+import Select from 'nui-v2/lib/select'
+// import Slider from 'nui-v2/lib/slider'
+// import Spinner from 'nui-v2/lib/spinner'
+// import Step from 'nui-v2/lib/step'
+// import Steps from 'nui-v2/lib/steps'
+// import Submenu from 'nui-v2/lib/submenu'
+import Switch from 'nui-v2/lib/switch'
+import TabPane from 'nui-v2/lib/tab-pane'
+import Table from 'nui-v2/lib/table'
+import TableColumn from 'nui-v2/lib/table-column'
+import Tabs from 'nui-v2/lib/tabs'
+import Tag from 'nui-v2/lib/tag'
+import TimePicker from 'nui-v2/lib/time-picker'
+// import TimeSelect from 'nui-v2/lib/time-select'
+import Tooltip from 'nui-v2/lib/tooltip'
+// import Transfer from 'nui-v2/lib/transfer'
+import Tree from 'nui-v2/lib/tree'
+// import Upload from 'nui-v2/lib/upload'
+
 export default {
+  // Alert,
+  // Aside,
+  // Autocomplete,
+  // Badge,
+  // Breadcrumb,
+  // BreadcrumbItem,
+  ButtonGroup,
+  // Calendar,
+  // Card,
+  // Carousel,
+  // CarouselItem,
+  // Cascader,
+  Checkbox,
+  // CheckboxButton,
+  CheckboxGroup,
+  Col,
+  Collapse,
+  CollapseItem,
+  // ColorPicker,
+  // Container,
+  // Countup,
+  DatePicker,
+  Dialog,
+  Dropdown,
+  DropdownItem,
+  DropdownMenu,
+  // Footer,
+  Form,
+  FormGrid,
+  FormItem,
+  // Header,
+  // Icon,
+  Input,
+  // InputNumber,
+  Loading,
+  // Main,
+  // Menu,
+  // MenuItem,
+  // MenuItemGroup,
+  Message,
+  MessageBox,
+  // Multi,
+  Notification,
+
+  // 自定义组件
+  NsButton,
+  NsCountDown,
+  NsPageCharts,
+  NsPageTable,
+  NsNoData,
+  NsSave,
+  NsSelect,
+  NsTableColumnOperateButton,
+  NsTableOperateButton,
+  // 自定义组件结束
+
+  Option,
+  OptionGroup,
+  Pagination,
+  Popover,
+  // Progress,
+  Radio,
+  RadioButton,
+  RadioGroup,
+  // Rate,
+  // RegionSelection,
+  Row,
+  Scrollbar,
+  Select,
+  // Slider,
+  // Spinner,
+  // Step,
+  // Steps,
+  // Submenu,
+  Switch,
+  TabPane,
+  Table,
+  TableColumn,
+  Tabs,
+  Tag,
+  TimePicker,
+  // TimeSelect,
+  Tooltip,
+  // Transfer,
+  Tree
+  // Upload
 }
