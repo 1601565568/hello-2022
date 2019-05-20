ed143b45c1851a51b192641ce3732ff42ea99011
涂海波 <1147104744@qq.com>
2019-05-20 13:49:31 +0800

    个人微信

--- src/views/third/personalWeChat.vue
+++ src/views/third/personalWeChat.vue
@@ -2,10 +2,10 @@
 <div class='person-page'>
   <div class='card flex flex-between'>
     <div>
-      <div class='fz28'>南讯软件</div>
-      <div class='fz36'>{{query.name}}</div>
-      <div class='fz28'>{{query.shopName}}  {{query.job==1?'店长':query.job==0?'导购':''}}</div>
-      <div class='fz28'>{{query.mobile}}</div>
+      <div class='fzi28'>南讯软件</div>
+      <div class='fzi36' style='margin-top:0.1rem'>{{query.name}}</div>
+      <div class='fzi28'>{{query.shopName}}  {{query.job==1?'店长':query.job==0?'导购':''}}</div>
+      <div class='fzi28' style='margin-top:0.1rem'>{{query.mobile}}</div>
     </div>
     <div class='person-img'>
       <img :src="query.image" alt="" class='img'>
@@ -63,12 +63,12 @@ export default {
 .loader-noCache{
   display :none
 }
-.fz28{
+.fzi28{
   color:#fff;
   font-size: 0.28rem;
   line-height: 0.42rem;
 }
-.fz36{
+.fzi36{
   color:#fff;
   font-size: 0.36rem;
   line-height: 0.55rem;
