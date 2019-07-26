ad175929bbec07dafa994d9e3246594f77d702d8
张仕风 <shifeng.zhang@nascent.cn>
2019-07-26 11:30:22 +0800

    fix #3369935 【正式】后台-会员列表中的会员类型多显示“会员”二字

--- src/views/Guide/Customer/NsTableGuide.vue
+++ src/views/Guide/Customer/NsTableGuide.vue
@@ -106,7 +106,7 @@
         <el-table-column prop="grade,memberCard" label="会员类型/卡号" align="left" >
           <template slot-scope="scope">
               <div v-if="scope.row.memberCard !==null || scope.row.grade !== null">
-                <span>{{(scope.row.grade === '0' || scope.row.grade === null) ? '非会员':'会员'+scope.row.grade}}/</span>
+                <span>{{(scope.row.grade === '0' || scope.row.grade === null) ? '非会员':scope.row.grade}}/</span>
                 <span>{{scope.row.memberCard === undefined ? '-':scope.row.memberCard}}</span>
                 <!-- {{(scope.row.grade === '0' || scope.row.grade === null) ? '-'+ ' / ' +scope.row.memberCard === undefined?'-':scope.row.memberCard:'会员' + scope.row.grade + ' / '+scope.row.memberCard}} -->
               </div>
