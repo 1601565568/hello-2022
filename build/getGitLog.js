8fbab05bc5b838f7682f0e690afbc94143822441
郑胜利 <675320203@qq.com>
2019-08-12 09:18:13 +0800

    删除多余文件

--- build/getGitLog.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-const { execSync } = require('child_process')
-const HEAD = 'git rev-parse HEAD'
-const REF = 'git symbolic-ref --short -q HEAD'
-process.env.VUE_APP_VERSION = `${execSync(REF).toString('utf8')}/${execSync(HEAD).toString('utf8')}/${new Date()}`.replace(/\n|\r|\t/g, '')
-
-module.exports = {
-}
