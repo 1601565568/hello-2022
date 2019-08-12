31fdbe836a14df909771fd2620ceea3c6c0e19d5
叶岩松 <yansong.ye@nascent.cn>
2019-08-12 14:57:35 +0800

    聊天界面优化

--- src/views/Guide/WxChat/index.vue
+++ src/views/Guide/WxChat/index.vue
@@ -70,7 +70,7 @@
     </ns-page-table>
 
     <el-container class="talk-chat__container">
-      <el-aside class="talk-aside" style="width: 22%;">
+      <el-aside class="talk-aside" style="width: 30%;">
         <div class="talk-aside__group">客户 / 群</div>
         <el-scrollbar ref="fullScreen" v-loading.lock="targetLoading" :element-loading-text="$t('prompt.loading')">
           <div class="talk-aside__item">
@@ -115,7 +115,9 @@
                   暂无数据
                 </template>
                 <template v-if="!isChatLoadEnd">
-                  <a @click="loadChatLog(false)">查看更多</a>
+                  <ns-button type="text primary" @click="loadChatLog(false)" class="talk-main__header--btn">
+                    查看更多
+                  </ns-button>
                 </template>
               </font>
             </div>
@@ -154,9 +156,8 @@
     <el-dialog title="查看详情" :visible.sync="dialogVisible" width="442px" class="detail-dialog">
       <div class="detail-dialog__content" v-if="currTargetIndex!=null && targetList[currTargetIndex]!=null">
         <el-form label-width="80px" class="detail-leftside">
-          <el-form-item label-width="32px">
+          <el-form-item :label="targetList[currTargetIndex].talkerName" label-width="70px" class="detail-leftside__username">
             <el-form-grid>
-              <span style="font-size: 14px;color:#909399">{{targetList[currTargetIndex].talkerName}}</span>
               <!-- 男生图标-->
               <i class="iconfont icon-nan1" v-if="targetList[currTargetIndex].gender===1"></i>
               <!-- 女生图标-->
@@ -259,6 +260,7 @@ export default index
       width: 100%;
       height: 100%;
       border-radius: 50%;
+      flex-shrink: 0;
     }
     @b main {
       @e header {
@@ -293,10 +295,11 @@ export default index
         width: 52px;
         height: 52px;
         cursor: pointer;
+        flex-shrink: 0;
       }
       @e uname {
-        flex: 1;
         margin-left: 18px;
+        padding-right: 72px;
       }
     }
     @b chosen {
@@ -305,17 +308,20 @@ export default index
     }
     @b right {
       padding: 20px;
-      width: 50%;
+      width: 100%;
       position: relative;
       float: right;
-      clear: both;
       @e headportrait {
         width: 52px;
         height: 52px;
-        float: right;
+        position: absolute;
+        top: 0;
+        right: 20px;
       }
       @e uname {
-        margin-right: 70px;
+        float: right;
+        margin-right: 72px;
+        padding-left: 72px;
       }
     }
     @b msg {
@@ -452,6 +458,11 @@ export default index
   }
   >>>.el-dialog__body {
     color: var(--theme-font-color-primary);
+  }
+  .detail-leftside__username >>> .el-form-item__label {
+    font-size: var(--default-font-size-middle);
+    color: var(--theme-font-color-primary);
+    font-weight: bold;
   }
   .icon-qun {
     color: var(--theme-color-white);
