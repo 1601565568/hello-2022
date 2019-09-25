90bd4918e626585b185ac26dfda7c4eb7e49db7c
蓝泉和 <quanhe.lan@nascent.cn>
2019-09-25 11:06:46 +0800

    修复会员列表根据导购查询会员详情报错的问题

--- src/views/Guide/Customer/src/NsTableGuide.js
+++ src/views/Guide/Customer/src/NsTableGuide.js
@@ -84,7 +84,7 @@ export default {
       if (this._data._table.data.length > 0) {
         this._data._table.data = []
       }
-      _this.offLineShopId = data.id
+      _this.offLineShopId = data.parentId != 0 ? data.parentId : data.id
       _this.shuJushuzu = data
       _this.loading = true
       _this.$reload().then(rep => {
