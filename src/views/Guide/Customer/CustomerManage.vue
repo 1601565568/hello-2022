aa7464b6de03659a70a9a22cdaa8518f92b434ec
蓝泉和 <quanhe.lan@nascent.cn>
2019-11-01 14:05:26 +0800

    会员详情弹窗增加取消事件

--- src/views/Guide/Customer/CustomerManage.vue
+++ src/views/Guide/Customer/CustomerManage.vue
@@ -818,7 +818,7 @@
         </el-tabs>
       </div>
       <span slot="footer">
-        <ns-button @click="shopKuhuShow==false">取 消</ns-button>
+        <ns-button @click="closeDetailDialog">取 消</ns-button>
       </span>
     </el-dialog>
     <!-- 打标签-->
