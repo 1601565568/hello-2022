d854c4938dfa268318e8327ca655f97c1cd81765
李兴 <xing.li@nascent.cn>
2019-12-17 18:09:25 +0800

    关键字不能输入表情调整

--- src/views/Guide/SgQuicklyWord/src/List.js
+++ src/views/Guide/SgQuicklyWord/src/List.js
@@ -85,6 +85,12 @@ export default {
                 } else if (this.model.keyWord.length > 25) {
                   callback(new Error('关键词长度在 25 以内'))
                 }
+                let arr = '😀😁😂🤣😃😄😅😆😉😊😋😎😍😘😭😢😬🙂🤗🤔😐😶🙄😏😣😥😮😪😫😴😌😇😜😝🤤😒😓🙃🤑😲🤐😖😤🤥🤧😧😨😱😳😡😷🤓👌👍😈👻💩🙈🙉🙊🐷🐸'.split('')
+                for (var i = 0; i < arr.length; i++) {
+                  if (value.indexOf(arr[i]) !== -1) {
+                    callback(new Error('不支持表情'))
+                  }
+                }
               }
               callback()
             }
