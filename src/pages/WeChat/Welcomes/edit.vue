02c0375b68dbd437a377318b7d539e936b897b64
黄巧玲 <qiaoling.huang@nascent>
2020-04-03 16:58:30 +0800

    style  去掉页面里卡片的边框

--- src/pages/WeChat/Welcomes/edit.vue
+++ src/pages/WeChat/Welcomes/edit.vue
@@ -2,7 +2,7 @@
   <div>
     <el-form ref="form" placement="right" label-width="100px" :model="model" :rules="rules">
         <ElScrollbar ref="fullScreen">
-          <div class="message-container">
+          <div class="message-container card-noborder">
             <ElCard shadow="never">
               <div>
                 <ElForm label-width="100px">
@@ -181,9 +181,11 @@ export default edit
   }
   /* 底部按钮样式 end*/
 
-  /* 卡片样式 start*/
-  >>> .el-card:last-child {
-    border-bottom: none;
+  /* 去点el-card的默认边框 start */
+  .card-noborder {
+    >>>.el-card {
+      border: 0;
+    }
   }
-  /* 卡片样式 end*/
+  /* 去点el-card的默认边框 end */
 </style>
--- src/pages/WeWork/WelcomeCode/Edit.vue
+++ src/pages/WeWork/WelcomeCode/Edit.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="card-noborder">
     <div class="page-title">
       {{ model.welcomeCodeUuid?'修改':'新增' }}智能欢迎语
     </div>
@@ -943,12 +943,6 @@ export default Edit
     border-bottom-right-radius: var(--default-radius-mini);
   }
   /* 底部按钮样式 end*/
-
-  /* 卡片样式 start*/
-  >>> .el-card:last-child {
-    border-bottom: none;
-  }
-  /* 卡片样式 end*/
   /* 迁移 */
   .hand {
     cursor: pointer;
@@ -972,4 +966,15 @@ export default Edit
   .span-red{
     color: #FF0000;
   }
+  >>> .el-card .el-card__header {
+    font-size: var(--default-font-size-base);
+    padding: var(--default-padding-larger) 20px;
+  }
+  /* 去点el-card的默认边框 start */
+  .card-noborder {
+    >>>.el-card {
+      border: 0;
+    }
+  }
+  /* 去点el-card的默认边框 end */
 </style>
