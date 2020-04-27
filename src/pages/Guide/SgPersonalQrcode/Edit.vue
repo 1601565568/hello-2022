8fcff523f786277837f40b98502757a94e44782b
黄巧玲 <qiaoling.huang@nascent>
2020-04-27 20:23:05 +0800

    fix #6541386 修改聚合二维码 编辑页面

--- src/pages/Guide/SgPersonalQrcode/Edit.vue
+++ src/pages/Guide/SgPersonalQrcode/Edit.vue
@@ -1,6 +1,6 @@
 <template>
   <div style="background-color: white">
-    <el-scrollbar ref="integralRuleHeight" outsider>
+    <el-scrollbar ref="fullScreen" outsider>
 
       <el-form label-width="150px" ref="form">
         <!-- 会员招募码配置开始 -->
@@ -42,7 +42,7 @@
             <ElFormItem :rules="rules">
               <div class="message-detail" >
                 <ElTable :data="tableData" class="message-detail__table">
-                  <ElTableColumn prop="style" label="名称" align="center" width="80">
+                  <ElTableColumn prop="style" label="名称">
                     <template slot-scope="scope">
                       {{ scope.row.name }}
                     </template>
@@ -52,7 +52,7 @@
                       {{ scope.row.userName?scope.row.userName:'-' }}({{ scope.row.userId?scope.row.userId:'-' }})
                     </template>
                   </ElTableColumn>
-                  <ElTableColumn prop="style" label="子码" align="center" width="150">
+                  <ElTableColumn prop="style" label="子码" align="center" width="70">
                     <template slot-scope="scope">
                       <img v-if="scope.row.image" :src="scope.row.image" width="50px" height="50px" class="company-upload__avatar">
                     </template>
@@ -69,7 +69,7 @@
 <!--                      <el-input v-model="scope.row.num" type="number" onkeyup="this.value=this.value.replace(/\D|^0/g,'')" onafterpaste="this.value=this.value.replace(/\D|^0/g,'')"></el-input>-->
                     </template>
                   </ElTableColumn>
-                  <ElTableColumn label="操作" align="center" :width="80">
+                  <ElTableColumn label="操作" align="center" :width="60">
                     <template slot-scope="scope">
                       <ns-button type="text" size="small" @click="handleDelete(scope)">删除</ns-button>
                     </template>
