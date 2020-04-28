c38f2d675a417d9d0781f462d9e16185e69e13dd
QiYu <lwe126629@163.com>
2020-04-28 11:51:12 +0800

    fix -> 社群运营-群运营-助手和自动回复

--- src/config/routes/social.ts
+++ src/config/routes/social.ts
@@ -1,99 +1,57 @@
 export default {
-  'path': '/Guide/SgGuide/List',
-  'name': 'SHOP_MANAGER',
+  'path': '/Social/SocialOperation',
+  'name': 'SocialOperation',
   'component': () => import('@nascent/ecrp-ecrm/src/layout/Default.vue'),
   'meta': {
-    'title': '门店管理',
+    'title': '社群运营',
     'requiresAuth': true
   },
   'children': [
     {
-      'path': '/Guide/SgGuide',
-      'name': 'SHOP_MANAGER2',
-      'title': '门店管理',
+      'path': '/Social/OperationConfig',
+      'name': 'OperationConfig',
+      'title': '运营设置',
       'component': () => import('@nascent/ecrp-ecrm/src/layout/Content.vue'),
       'children': [
         {
-          'path': '/Guide/SgStoreManagement/List',
-          'name': 'ShopManager',
-          'title': '门店列表',
+          'path': '/Social/OperationConfig',
+          'name': 'OperationConfig',
+          'title': '企业微信设置',
           'component': () => import('@/pages/Guide/SgStoreManagement/List.vue')
         }
       ]
     },
     {
-      'path': '/Guide/SgGuide',
-      'name': 'YGGL',
-      'title': '员工管理',
+      'path': '/Social/WeWorkGroup',
+      'name': 'WeWorkGroup',
+      'title': '群运营',
       'component': () => import('@nascent/ecrp-ecrm/src/layout/Content.vue'),
       'children': [
         {
-          'path': '/Guide/SgGuide/List',
-          'name': 'Guide_List',
-          'title': '员工列表',
-          'component': () => import('@/pages/Guide/SgGuide/List.vue')
+          'path': '/Social/WeWorkGroup/GroupAssistant',
+          'name': 'WeWorkGroupAssistant',
+          'title': '群助手',
+          'component': () => import('@/pages/Social/WeWorkGroup/WeWorkGroupWelcomeCode/index.vue')
         },
         {
-          'path': '/Guide/SgMoreAccount/List',
-          'name': 'GuideList',
-          'title': '配置多谋个人号',
-          'component': () => import('@/pages/Guide/SgMoreAccount/List.vue')
-        },
-        {
-          'path': '/Guide/GuideSecretary/List',
-          'name': 'GuideSecretary_list',
-          'title': '导购秘书',
-          'component': () => import('@/pages/Guide/GuideSecretary/List.vue')
-        },
-        {
-          'path': '/Guide/SgShopGroup/index',
-          'name': 'SgShopGroup_index',
-          'title': '门店群',
-          'component': () => import('@/pages/Guide/SgShopGroup/index.vue')
-        },
-        {
-          'path': '/Guide/SgShopGroupRobot/index',
-          'name': 'SgShopGroupRobot_index',
-          'title': '门店群AI机器人',
-          'component': () => import('@/pages/Guide/SgShopGroupRobot/index.vue')
+          'path': '/Social/WeWorkGroup/GroupWelcomeCode',
+          'name': 'WeWorkGroupWelcomeCode',
+          'title': '群欢迎语',
+          'component': () => import('@/pages/Social/WeWorkGroup/WeWorkGroupAssistant/index.vue')
         }
       ]
     },
     {
-      'path': '/Guide/SgGuide',
-      'name': 'CustomerManage',
-      'title': '客户管理',
+      'path': '/Social/OperationData',
+      'name': 'OperationData',
+      'title': '运营数据',
       'component': () => import('@nascent/ecrp-ecrm/src/layout/Content.vue'),
       'children': [
         {
-          'path': '/Guide/Customer/CustomerManage',
-          'name': 'CustomerList',
-          'title': '客户列表',
+          'path': '/Social/OperationData/GuideBehaviorCount',
+          'name': 'GuideBehaviorCount',
+          'title': '导购行为数据统计',
           'component': () => import('@/pages/Guide/Customer/CustomerManage.vue')
-        },
-        {
-          'path': '/Guide/Customer/FriendManage',
-          'name': 'FriendData',
-          'title': '好友数据',
-          'component': () => import('@/pages/Guide/FriendData/List.vue')
-        },
-        {
-          'path': '/Guide/GroupData/index',
-          'name': 'GroupData',
-          'title': '群数据',
-          'component': () => import('@/pages/Guide/GroupData/index.vue')
-        },
-        {
-          'path': '/Guide/transCustomerRecord',
-          'name': 'TRANS_CUSTOMER_RECORD',
-          'title': '客户转移记录',
-          'component': () => import('@/pages/Guide/SgCustomer/NsTableSgCustomer.vue')
-        },
-        {
-          'path': '/guide/customer/getCustomerConfig',
-          'name': 'APPLET_TEMPLATE',
-          'title': '客户管理配置',
-          'component': () => import('@/pages/Guide/customerConfig/index.vue')
         }
       ]
     }
