9cc11f45f98dbf390aa8ccce8692376873123c2b
liuhonglei <15038088414@163.com>
2020-05-11 10:30:36 +0800

    快捷话术 批量按钮正确显示 删除分类后刷新话术列表

--- src/pages/Guide/SgQuicklyWord/List.vue
+++ src/pages/Guide/SgQuicklyWord/List.vue
@@ -81,7 +81,7 @@
 <!--            <el-table-column prop="keyWord" class-name="keyword" width="130" :show-overflow-tooltip="true" label="关键词" align="left"></el-table-column>-->
             <el-table-column prop="content" label="话术内容" :show-overflow-tooltip="true" align="left"></el-table-column>
             <el-table-column prop="name" label="分类" align="left"></el-table-column>
-            <el-table-column prop="createTime" label="添加时间" align="center"></el-table-column>
+            <el-table-column prop="createTime" label="创建时间" align="center"></el-table-column>
             <el-table-column align="left" v-if="showOrder">
               <template slot="header">
                 排序
