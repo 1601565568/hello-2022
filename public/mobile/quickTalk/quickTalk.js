265f4f59e0795997d35b1de6758fc6a27533d04e
liuhonglei <15038088414@163.com>
2020-07-27 14:35:52 +0800

    fix->快捷话术-> 手动处理回车换行符更改替换字符

--- public/mobile/quickTalk/quickTalk.js
+++ public/mobile/quickTalk/quickTalk.js
@@ -104,13 +104,13 @@ function cancelSend () {
 function sendWord () {
   let id = $('.item__radio--selected').attr('id').replace(/[^0-9]/ig, '')
   let content = $('#word' + id).text()
-  window.console.log('未替换转义换行符=>' + codeCompile(content))
   // let text = content.replace(/\n/g, codeCompile('<br>'))
-  let text = content.replace(/\\n/g, '\\n')
-  text = content.replace(/\n/g, '\\n')
+  // let text = content.replace(/\\n/g, '\\n')
+  window.console.log('替换前=>' + content)
+  let text = content.replace(/\n/g, '%5cn')
   // String.raw({ raw: content } ,0, 1, 2)
-  let url = '{"type":1,"content":"' + content + '","exit":true}'
-  window.console.log('转义换行符=>' + text)
+  let url = '{"type":1,"content":"' + text + '","exit":true}'
+  window.console.log('替换后=>' + text)
   window.location.href = host + url
 }
 /**
