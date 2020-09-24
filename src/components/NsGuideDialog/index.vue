6f571bcca1d50405b965df36de9dba23cf88c80d
吴梦芹 <mengqin.wu@nascent.cn>
2020-09-24 15:52:52 +0800

    代码合并问题

--- src/components/NsGuideDialog/index.vue
+++ src/components/NsGuideDialog/index.vue
@@ -26,7 +26,7 @@
     <div class="template-search__chooes" v-if="!isButton" :type="type" @click="onDialogOpen()"><Icon v-if="type === 'text'" type="plus"/>{{btnTitle}}</div>
     <NsButton v-if="isButton" :type="type" @click="onDialogOpen()"><Icon v-if="type === 'text'" type="plus"/>{{btnTitle}}</NsButton>
     <el-dialog :title="dialogTitle" :visible.sync="visible" :show-scroll-x="false"
-               :close-on-click-modal = "false" :before-close="onDialogClose" width="1100"><!-- 按员工设置使用范围时，所选员工会优先选择使用该条欢迎语而非归属门店设置的欢迎语 -->
+               :close-on-click-modal = "false" :before-close="onDialogClose" width="1100px"><!-- 按员工设置使用范围时，所选员工会优先选择使用该条欢迎语而非归属门店设置的欢迎语 -->
       <div slot="title">
         {{dialogTitle}}
         <el-tooltip  content="按员工设置使用范围时，所选员工会优先选择使用该条欢迎语而非归属门店设置的欢迎语">
@@ -143,6 +143,24 @@ export default index
 
 <style scoped>
   @import "@theme/variables.pcss";
+  .template-search__box {
+    width: 182px;
+    height: 28px;
+    background: #FFFFFF;
+    border: 1px solid #DCDFE6;
+    border-radius: 3px;
+    border-radius: 3px;;
+    display: flex;
+  span{
+    width: 141px;
+    height: 27px;
+    margin-left: 10px;
+    border-right: 1px solid #DCDFE6;;
+  }
+  > div + span {
+    margin-left: var(--default-margin-small);
+  }
+  }
   .template-search__chooes{
     cursor: pointer;
     width: 40px;
