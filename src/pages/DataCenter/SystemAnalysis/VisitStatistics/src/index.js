71f1fac43dd8963ee13c57b5c05aa2a33fdfbd2a
郑文秀 <1871364480@qq.com>
2020-10-12 16:28:09 +0800

    系统分析，用户分析页面搭建完成

---
+++ src/pages/DataCenter/SystemAnalysis/VisitStatistics/src/index.js
@@ -0,0 +1,59 @@
+import tableMixin from '@nascent/ecrp-ecrm/src/mixins/table'
+export default {
+  mixins: [tableMixin],
+  data () {
+    return {
+      form: {
+        data: '', // 日期
+        staff: '' // 员工
+      },
+      pickerOptions: {
+        shortcuts: [
+          {
+            text: '最近一周',
+            onClick (picker) {
+              const end = new Date()
+              const start = new Date()
+              start.setTime(start.getTime() - 3600 * 1000 * 24 * 7)
+              picker.$emit('pick', [start, end])
+            }
+          },
+          {
+            text: '最近一个月',
+            onClick (picker) {
+              const end = new Date()
+              const start = new Date()
+              start.setTime(start.getTime() - 3600 * 1000 * 24 * 30)
+              picker.$emit('pick', [start, end])
+            }
+          },
+          {
+            text: '最近三个月',
+            onClick (picker) {
+              const end = new Date()
+              const start = new Date()
+              start.setTime(start.getTime() - 3600 * 1000 * 24 * 90)
+              picker.$emit('pick', [start, end])
+            }
+          }
+        ]
+      },
+      options: [{
+        value: '选项1',
+        label: '黄金糕'
+      }, {
+        value: '选项2',
+        label: '双皮奶'
+      }, {
+        value: '选项3',
+        label: '蚵仔煎'
+      }, {
+        value: '选项4',
+        label: '龙须面'
+      }, {
+        value: '选项5',
+        label: '北京烤鸭'
+      }]
+    }
+  }
+}
