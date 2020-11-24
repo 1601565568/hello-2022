d10cec1920b89ad8cd91add543bd773857c48fb4
胡盛懿 <shengyi.hu@nascent.cn>
2020-11-24 16:54:45 +0800

    一客一码bug

--- src/pages/Guide/CustomerCode/components/Analysis/FriendTable.vue
+++ src/pages/Guide/CustomerCode/components/Analysis/FriendTable.vue
@@ -204,8 +204,11 @@ export default {
     }
   },
   watch: {
-    startTime (newVal) {
-      this.changeSearchfrom({ timeStart: newVal, timeEnd: this.endTime })
+    startTime: {
+      handler (newVal) {
+        this.changeSearchfrom({ timeStart: newVal, timeEnd: this.endTime })
+      },
+      immediate: true
     }
   }
 }
