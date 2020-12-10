fe44cd05d0948c584863358b747b21ffba056cf9
胡盛懿 <shengyi.hu@nascent.cn>
2020-12-10 15:19:22 +0800

    拦截菜单新增智能欢迎语

--- src/constants/interceptRouter.ts
+++ src/constants/interceptRouter.ts
@@ -1,4 +1,5 @@
 const customCode = ['CustomerCode', 'CustomerCodeList', 'CustomerCodeEdit', 'CustomerCodeAnalysis'] // 一客一码
 const WeWorkGroupWelcomeCode = ['WeWorkGroupWelcomeCode'] // 群欢迎语
 const chatRoomGroup = ['chatRoomGroupAll', 'chatRoomGroup', 'chatRoomGroupAddOrEdit'] // 群聚合码
-export default [...customCode, ...WeWorkGroupWelcomeCode, ...chatRoomGroup] // 需要拦截的路由
+const WelcomeCodeList = ['WelcomeCodeList', 'WelcomeCodeEdit'] // 智能欢迎语
+export default [...customCode, ...WeWorkGroupWelcomeCode, ...chatRoomGroup, ...WelcomeCodeList] // 需要拦截的路由
