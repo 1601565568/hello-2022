2e1c5b100dd9546a4cde3738a7c4e6ced245b78d
张志强 <zhiqiang.zhang@nascent.cn>
2021-02-15 10:37:28 +0800

    fix>修改会话存档功能菜单位置

--- src/config/routes/chatManage.ts
+++ src/config/routes/chatManage.ts
@@ -13,24 +13,7 @@ export default {
       'title': '会话管理',
       'component': () => import('@nascent/ecrp-ecrm/src/layout/Content.vue'),
       'children': [
-        {
-          'path': '/Social/WeWorkGroup/ChatRecord',
-          'name': 'ChatRecord',
-          'title': '聊天记录',
-          'component': () => import('@/pages/Social/ChatRecord/index.vue')
-        },
-        {
-          'path': '/Social/WeWorkGroup/sensitiveWords',
-          'name': 'sensitiveWords',
-          'title': '敏感词监控',
-          'component': () => import('@/pages/WeWork/SensitiveWords/index.vue')
-        },
-        {
-          'path': '/Social/OperationConfig/topicAnalysis',
-          'name': 'topicAnalysis',
-          'title': '话题分析 ',
-          'component': () => import('@/pages/WeWork/topicAnalysis/index.vue')
-        }
+
       ]
     }
   ]
