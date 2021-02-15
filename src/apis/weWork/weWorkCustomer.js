fb9740261ddcd946719defe51859ef3be4aeedc1
张俊强 <910199862@qq.com>
2021-02-15 09:45:05 +0800

    好友分析

--- src/apis/weWork/weWorkCustomer.js
+++ src/apis/weWork/weWorkCustomer.js
@@ -33,5 +33,33 @@ export default {
   getRepeatGuideList: {
     url: '/wework/repeat/getRepeatGuideList',
     method: 'get'
+  },
+  /**
+   * @msg: 获取客户分析顶部数据
+   */
+  queryAnalysisOverview: {
+    url: '/weWork/friendsAnalysis/queryAnalysisOverview',
+    method: 'post'
+  },
+  /**
+   * @msg: 获取分析列表-日期
+   */
+  queryAnalysisListByDate: {
+    url: '/weWork/friendsAnalysis/queryAnalysisListByDate',
+    method: 'post'
+  },
+  /**
+   * @msg: 获取分析列表-日期
+   */
+  queryAnalysisListByDateReturnList: {
+    url: '/weWork/friendsAnalysis/queryAnalysisListByDateReturnList',
+    method: 'post'
+  },
+  /**
+   * @msg: 获取分析列表-成员
+   */
+  queryAnalysisListByUser: {
+    url: '/weWork/friendsAnalysis/queryAnalysisListByUser',
+    method: 'post'
   }
 }
