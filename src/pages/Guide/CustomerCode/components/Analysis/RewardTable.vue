9ba7b5ec2fbb904ba2a52b70846c42d1b69021f7
郑文秀 <1871364480@qq.com>
2021-03-03 10:07:39 +0800

    dev -> 文案修改

--- src/pages/Guide/CustomerCode/components/Analysis/RewardTable.vue
+++ src/pages/Guide/CustomerCode/components/Analysis/RewardTable.vue
@@ -23,7 +23,7 @@
         </el-select>
       </el-form-item>
       <el-form-item label="">
-        <el-input v-model="seachVal" placeholder="请输入员工姓名"  @keyup.enter.native="handleSearch">
+        <el-input v-model="seachVal" placeholder="请输入推广大师昵称"  @keyup.enter.native="handleSearch">
           <Icon type="ns-search-copy" slot="suffix" class='search-icon' @click="handleSearch"></Icon>
         </el-input>
       </el-form-item>
