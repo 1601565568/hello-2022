0aea722ab09685301a1e9274bd60eb5b99483e88
胡盛懿 <15858115427@163.com>
2021-07-13 15:49:06 +0800

    门店组件优化

--- src/components/NsRoomDialog/index.vue
+++ src/components/NsRoomDialog/index.vue
@@ -173,6 +173,7 @@ export default {
       model: {
         workShopId: '',
         departmentId: '',
+        shopAreaId: '',
         name: '',
         ownerName: ''
       },
@@ -190,6 +191,7 @@ export default {
     shopAreaId: function (o1, o2) {
       let shopOptions = []
       this.shopId = ''
+      this.model.shopAreaId = o1.value
       if (!o1.value || o1.value !== o2.value) {
         if (o1.value === 0) {
           this.shops = this.allShops
@@ -223,6 +225,7 @@ export default {
       this.model = {
         workShopId: '',
         departmentId: '',
+        shopAreaId: '',
         name: '',
         ownerName: ''
       }
@@ -264,6 +267,7 @@ export default {
       this.model = {
         workShopId: '',
         departmentId: '',
+        shopAreaId: '',
         name: '',
         ownerName: ''
       }
