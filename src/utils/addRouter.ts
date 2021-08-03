f4f26fe5da7e5befc607613c3e9c856365dda310
胡盛懿 <15858115427@163.com>
2021-08-03 20:45:49 +0800

    bug修复

--- src/utils/addRouter.ts
+++ src/utils/addRouter.ts
@@ -1,4 +1,4 @@
-export default (parentName: any, name: any, module: any) => (
+export default (parentName: any, secondName:any, name:any, module: any) => (
   {
     path: `/${module}`,
     name: parentName,
@@ -10,7 +10,7 @@ export default (parentName: any, name: any, module: any) => (
     children: [
       {
         path: `/${module}/Content`,
-        name: `${module}`,
+        name: secondName,
         title: '系统使用分析',
         meta: { requiresAuth: true },
         component: () => import('@nascent/ecrp-ecrm/src/layout/Content.vue'),
@@ -30,3 +30,15 @@ export default (parentName: any, name: any, module: any) => (
     ]
   }
 )
+export const setRouter = (module: string, parentName: string, secondName?: string) => (
+  {
+    path: `/${module}/Content/Index`,
+    name: parentName + secondName + module,
+    meta: { requiresAuth: true },
+    title: '系统使用分析',
+    'component': () => import(`@/pages/${module}/index.vue`)
+  }
+)
+export const thirdAuthRouter = (parentNamame: string, secondName?: string) => setRouter('ThirdAuth', parentNamame, secondName)
+export const greetingRouter = (parentNamame: string, secondName?: string) => setRouter('Greeting', parentNamame, secondName)
+export const setAllRouter = (parentNamame:string, secondName?: string) => [thirdAuthRouter(parentNamame, secondName), greetingRouter(parentNamame, secondName)]
