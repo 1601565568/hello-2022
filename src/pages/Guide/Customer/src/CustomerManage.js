9c7b2744617cfd2a54e4c0e052b6eeae86554684
haixin <1367228921@qq.com>
2021-09-17 13:41:33 +0800

    添加正在更换交互

--- src/pages/Guide/Customer/src/CustomerManage.js
+++ src/pages/Guide/Customer/src/CustomerManage.js
@@ -1007,6 +1007,9 @@ export default {
         }
       }
       this.createCustomerTransferTask(params, taskType)
+      if (taskType === 1) {
+        this.$refs.remindMsg.showMsg()
+      }
     },
     // sgExclusiveGuideId: this.formatSgExclusiveGuideId(removeCheckList, addcheckList),
     // 格式化勾选参数数组
