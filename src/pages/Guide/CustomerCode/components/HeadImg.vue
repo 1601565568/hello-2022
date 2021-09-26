599260b94efe782d29f852c99ce165d02291b6a3
haixin <1367228921@qq.com>
2021-09-26 15:32:44 +0800

    头像样式修改

--- src/pages/Guide/CustomerCode/components/HeadImg.vue
+++ src/pages/Guide/CustomerCode/components/HeadImg.vue
@@ -11,16 +11,14 @@
         class="larger-item"
       >
         <div class="radio-view">
-          <el-radio
-            v-model="gender"
-            :label="1"
-          >方形
-          </el-radio>
-          <el-radio
-            v-model="gender"
-            :label="0"
-          >圆形
-          </el-radio>
+          <div class="base-view" @click="genderClick(0)">
+            <span :class="gender == 0 ? 'iconfont icon-ns_arrow-circle-leftfuben6 icon-color': 'iconfont icon-ns-succeed'"></span>
+            <img :class="gender == 0 ? 'base-view-img base-view-img-sel' : 'base-view-img'" src="@/assets/default-avatar.png">
+          </div>
+          <div class="base-view" @click="genderClick(1)">
+            <span :class="gender == 1 ? 'iconfont icon-ns_arrow-circle-leftfuben6 icon-color': 'iconfont icon-ns-succeed'"></span>
+            <img :class="gender == 1 ? 'base-view-img base-view-img-radius base-view-img-sel' : 'base-view-img base-view-img-radius'"  src="@/assets/default-avatar.png">
+          </div>
         </div>
       </el-form-item>
     </el-form>
@@ -32,12 +30,17 @@ export default {
   name: 'headeImg',
   data () {
     return {
-      gender: 1,
+      gender: null,
       rules: {
         name: [
           { required: true, message: '请选择头像样式', trigger: ['blur', 'change'] }
         ]
       }
+    }
+  },
+  methods: {
+    genderClick (index) {
+      this.gender = index
     }
   }
 }
@@ -45,26 +48,34 @@ export default {
 
 <style scoped>
 .radio-view {
+  display: flex;
+  flex-direction: row;
+}
+.base-view {
+  display: flex;
+  flex-direction: row;
+  height: 48px;
+  margin-right: 20px;
+}
 
+.base-view-img {
+  width: 48px;
+  height: 48px;
+  border: 1px solid #FFFFFF;
+  border-radius: 4px;
+  margin-left: 8px;
+}
+.base-view-img-sel {
+  border: 1px solid #0094FC;
+}
+.base-view-img-radius {
+  border-radius: 50%;
 }
 .item-view {
   margin-top: 24px;
   margin-bottom: 24px;
 }
-.logo-type {
-  height: 24px;
-  width: 24px;
-  border: 1px solid #d9d9d9;
-  background-color: #fff;
-  display: inline-block;
-  &.active {
-    border-color: #41a2e8;
-  }
-  &.circle {
-    border-radius: 50%;
-  }
-  &.square {
-    border-radius: 2px;
-  }
+.icon-color {
+  color: #0094FC;
 }
 </style>
