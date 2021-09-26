652902f12fc8ae76958d223c2f783c855b9f58c1
haixin <1367228921@qq.com>
2021-09-26 10:59:52 +0800

    分享模块修改

--- src/pages/Guide/CustomerCode/components/Register.vue
+++ src/pages/Guide/CustomerCode/components/Register.vue
@@ -1,12 +1,56 @@
 <template>
-  <div>注册会员</div>
+  <div class="item-view">
+    <el-form
+      label-width="100px"
+      label-position="left"
+      size="medium"
+      class="normal-from">
+      <el-form-item
+        label="注册会员"
+        class="larger-item"
+      >
+        <drap-upload
+          tip=""
+          v-model="image"
+          :maxWidth="750"
+          :maxHeight="1334"
+          :showPont="false"
+          :maxSize="1"
+          :isNeedCrop="true"
+          :showFooter="false"
+        >
+        </drap-upload>
+        <div class="remind-text">恢复默认引导图</div>
+      </el-form-item>
+    </el-form>
+  </div>
 </template>
 
 <script>
+import DrapUpload from '@/components/NewUi/DrapUpload'
 export default {
-  name: 'register'
+  name: 'register',
+  components: {
+    DrapUpload
+  },
+  data () {
+    return {
+      image: ''
+    }
+  }
 }
 </script>
 
-<style lang="sass" scoped>
+<style scoped>
+@import '../styles/reset.css';
+.item-view {
+  margin-top: 24px;
+  margin-bottom: 24px;
+}
+.remind-text {
+  margin-top: 8px;
+  font-size: 14px;
+  color: #0392FB;
+  line-height: 22px;
+}
 </style>
--- src/pages/Guide/CustomerCode/components/Share.vue
+++ src/pages/Guide/CustomerCode/components/Share.vue
@@ -1,63 +1,85 @@
 <template>
-  <div class="share-view">
-    <el-form>
+  <div class="item-view">
+    <el-form
+      :rules="rules"
+      label-width="100px"
+      label-position="left"
+      size="medium"
+      class="normal-from">
       <el-form-item
         label="分享按钮颜色"
-        class="item-view"
+        class="larger-item"
       >
+      <div class="color-view">
         <el-color-picker
           v-model="nickColour"
-          :disabled="isStating"
         ></el-color-picker>
-        <span>重置</span>
+        <span class="color-text">重置</span>
+      </div>
       </el-form-item>
       <el-form-item
         label="分享按钮名称"
-        required
-        class="item-view"
+        class="larger-item"
+        prop="shareText"
       >
-      <length-input
-        v-model="shareName"
-        placeholder="请输入名称"
-        :length="20"
-        :disabled="isStating"
-      />
+        <length-input
+          v-model="shareText"
+          placeholder="请输入分享按钮名称"
+          :length="10"
+        />
       </el-form-item>
     </el-form>
   </div>
 </template>
 
 <script>
-import ElColorPicker from '@nascent/nui/lib/color-picker'
+import DrapUpload from '@/components/NewUi/DrapUpload'
 import LengthInput from '@/components/NewUi/LengthInput'
+import ElColorPicker from '@nascent/nui/lib/color-picker'
 export default {
   name: 'share',
   components: {
-    ElColorPicker,
-    LengthInput
+    LengthInput,
+    ElColorPicker
   },
   data () {
     return {
-      nickColour: 'black',
-      isStating: false,
-      shareName: ''
+      shareText: '立即分享',
+      nickColour: '#262626',
+      rules: {
+        shareText: [
+          { required: true, message: '请输入分享按钮名称', trigger: ['blur', 'change'] }
+        ]
+      }
     }
   }
 }
 </script>
 
 <style scoped>
-.share-view {
-  width: 100%;
-}
+@import '../styles/reset.css';
 .item-view {
+  margin-top: 24px;
+  margin-bottom: 24px;
+}
+.color-view {
   display: flex;
+  align-items: center;
+  flex-direction: row;
+}
+.color-text {
   font-size: 14px;
-  color: #595959;
+  color: #0094FC;
+  line-height: 20px;
+  display: inline-block;
+  margin-left: 16px;
 }
-.share-view {
-  >>> .el-form-item--small.el-form-item  {
-    margin-bottom: 0;
-  }
+
+.goods-view {
+  background: #F5F5F5;
 }
+.input-view {
+  padding: 16px 16px 0 16px;
+}
+
 </style>
