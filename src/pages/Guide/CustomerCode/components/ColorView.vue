ccad1eb17caa510a44468e44afb6ecd8f4163cb4
haixin <1367228921@qq.com>
2021-09-26 16:26:03 +0800

    默认颜色选择修改

--- src/pages/Guide/CustomerCode/components/ColorView.vue
+++ src/pages/Guide/CustomerCode/components/ColorView.vue
@@ -11,11 +11,13 @@
       >
       <div class="color-view">
         <div class="color-sel">
-          <div v-for="(item,index) in colorList" :key="index">
+          <div v-for="(item,index) in colorList.slice(0, 3)" :key="index">
             <div :class="colorIndex === index ? 'color-sel-base color-sel-base-user': 'color-sel-base'" @click="colorClick(index)">
-              <div :style="{background:item.one}" class="color-base"></div>
-              <div :style="{background:item.two}" class="color-base"></div>
-              <div :style="{background:item.three}" class="color-base"></div>
+              <div class="color-sel-base-cont">
+                <div :style="{background:item.one}" class="color-base"></div>
+                <div :style="{background:item.two}" class="color-base"></div>
+                <div :style="{background:item.three}" class="color-base"></div>
+              </div>
             </div>
           </div>
         </div>
@@ -23,6 +25,32 @@
       </div>
       </el-form-item>
     </el-form>
+    <el-dialog
+      title="选择配色方案"
+      :visible.sync="dialogVisible"
+      width="720px">
+      <div>
+        <div class="dialog-subtitle">选择配色方案</div>
+        <div class="color-sel dialog-color-sel">
+          <div v-for="(item,index) in colorList" :key="index">
+            <div :class="dialogIndex === index ? 'color-sel-base dialog-color-sel-base color-sel-base-user': 'color-sel-base dialog-color-sel-base'" @click="dialogIndexClick(index)">
+              <div class="color-sel-base-cont">
+                <div :style="{background:item.one}" class="color-base"></div>
+                <div :style="{background:item.two}" class="color-base"></div>
+                <div :style="{background:item.three}" class="color-base"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div>
+        <div class="dialog-subtitle">自定义配色方案</div>
+      </div>
+      <span slot="footer" class="dialog-footer">
+        <ns-button @click="dialogVisible = false">保存该配色</ns-button>
+        <ns-button type="primary" @click="dialogVisible = false">使用该配色</ns-button>
+      </span>
+    </el-dialog>
   </div>
 </template>
 
@@ -32,6 +60,7 @@ export default {
   data () {
     return {
       colorIndex: 0,
+      dialogIndex: null,
       colorList: [
         {
           one: '#FFB6C1',
@@ -47,8 +76,34 @@ export default {
           one: '#FFB6C1',
           two: '#FFC0CB',
           three: '#DC143C'
+        },
+        {
+          one: '#FFB6C1',
+          two: '#FFC0CB',
+          three: '#DC143C'
+        },
+        {
+          one: '#FFB6C1',
+          two: '#FFC0CB',
+          three: '#DC143C'
+        },
+        {
+          one: '#FFB6C1',
+          two: '#FFC0CB',
+          three: '#DC143C'
+        },
+        {
+          one: '#FFB6C1',
+          two: '#FFC0CB',
+          three: '#DC143C'
+        },
+        {
+          one: '#FFB6C1',
+          two: '#FFC0CB',
+          three: '#DC143C'
         }
-      ]
+      ],
+      dialogVisible: false
     }
   },
   methods: {
@@ -56,6 +111,10 @@ export default {
       this.colorIndex = index
     },
     getMoreColor () {
+      this.dialogVisible = true
+    },
+    dialogIndexClick (index) {
+      this.dialogIndex = index
     }
   }
 }
@@ -80,15 +139,31 @@ export default {
   display: flex;
   flex-direction: row;
 }
+.dialog-color-sel {
+  flex-wrap: wrap;
+}
 
 .color-sel-base {
-  display: flex;
-  flex-direction: row;
   width: 148px;
   height: 60px;
   border: 2px solid #EEEEEE;
   border-radius: 4px;
   margin-right: 16px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.color-sel-base-cont {
+  display: flex;
+  flex-direction: row;
+  width: 132px;
+  height: 44px;
+  border: 2px solid #979797;
+  border-radius: 2px;
+}
+
+.dialog-color-sel-base {
+  margin-bottom: 16px;
 }
 .color-sel-base-user {
   border: 2px solid #0392FB;
@@ -96,5 +171,11 @@ export default {
 .color-base {
   width: 33.33%;
   height: 100%;
+}
+.dialog-subtitle {
+  font-size: 14px;
+  color: #595959;
+  line-height: 22px;
+  margin-bottom: 16px;
 }
 </style>
