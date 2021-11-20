7a44c928293472d326310c077f76dc20edc6e981
haixin <1367228921@qq.com>
2021-11-20 16:45:27 +0800

    字段缺失校验

--- src/pages/WeWork/MaterialOperatChat/components/DataList.vue
+++ src/pages/WeWork/MaterialOperatChat/components/DataList.vue
@@ -93,17 +93,17 @@
                       placement="top-start"
                       width="300"
                       trigger="hover"
-                      :disabled="scope.row.shopName.length <= 10"
+                      :disabled="scope.row.shopName && scope.row.shopName.length <= 10"
                     >
-                      <div>{{ scope.row.shopName }}</div>
+                      <div>{{ scope.row.shopName || '-'}}</div>
                       <span
                         slot="reference"
-                        v-if="scope.row.shopName.length <= 10"
-                        >{{ scope.row.shopName }}</span
+                        v-if="scope.row.shopName && scope.row.shopName.length <= 10"
+                        >{{ scope.row.shopName || '-'}}</span
                       >
                       <span
                         slot="reference"
-                        v-if="scope.row.shopName.length > 10"
+                        v-if="scope.row.shopName && scope.row.shopName.length > 10"
                         >{{ scope.row.shopName.substr(0, 10) + '...' }}</span
                       >
                     </el-popover>
