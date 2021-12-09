4179312da2055694598b6b1fc37d6cf3daed841b
mozhimin <mozhimin1993@163.com>
2021-12-09 11:47:02 +0800

    fix(裂变大师): 新增裂变大师分析报表

--- src/pages/Guide/CustomerCode/components/Analysis/ReceiveTable.vue
+++ src/pages/Guide/CustomerCode/components/Analysis/ReceiveTable.vue
@@ -1,7 +1,7 @@
 <template>
   <page-table :searchCol='24'>
     <template slot='search'>
-      <el-form :inline="true" class='form-inline_top' @submit.native.prevent>
+      <el-form :inline="true" class='form-inline_top'>
         <el-form-item label="所属员工：">
           <NsGuideDialog :selfBtn='true' :appendToBody='true' :isButton="false" :auth="false" type="primary" btnTitle="" dialogTitle="选择员工" v-model="guideIds" @input="handleChangeGuide">
             <template slot='selfBtn'>
@@ -12,8 +12,8 @@
             </template>
           </NsGuideDialog>
         </el-form-item>
-        <el-form-item label="领取状态：" class='el-form__change'>
-          <el-select v-model="model.presentsState" placeholder="请选择" @change='(value)=>{changeSearchfrom({status:value})}'>
+        <el-form-item label="奖励类型：" class='el-form__change'>
+          <el-select v-model="model.status" placeholder="请选择" @change='(value)=>{changeSearchfrom({prizeType:value})}'>
             <el-option
               v-for="item in statusList"
               :key="item.value"
@@ -22,9 +22,25 @@
             </el-option>
           </el-select>
         </el-form-item>
+        <el-form-item label="奖励内容：" @keyup.enter.native="handleSearch">
+          <el-input v-model="prizeName" placeholder="请输入奖励内容">
+          </el-input>
+        </el-form-item>
+        <el-form-item label="领取时间：" class='el-form__change'>
+          <el-date-picker
+            v-model="seachDate"
+            type="datetimerange"
+            value-format="yyyy-MM-dd HH:mm:ss"
+            range-separator="至"
+            start-placeholder="请选择开始日期"
+            end-placeholder="请选择结束日期"
+            :default-time="['00:00:00','23:59:59']"
+            align="right">
+          </el-date-picker>
+        </el-form-item>
         <el-form-item label="">
           <el-input v-model="seachVal" placeholder="请输入裂变大师昵称"  @keyup.enter.native="handleSearch">
-            <Icon type="ns-search-copy" slot="suffix" class='search-icon' @click="handleSearch"></Icon>
+            <Icon type="ns-search" slot="suffix" class='search-icon' @click="handleSearch"></Icon>
           </el-input>
         </el-form-item>
       </el-form>
@@ -38,59 +54,69 @@
           :data="_data._table.data"
           class="new-table_border"
           v-loading.lock="_data._table.loadingtable"
+          :row-style="tableRowClassName"
           @sort-change="handleSort"
           style="width: 100%">
           <el-table-column
-            prop="name"
+            prop="employeeName"
             label="裂变大师">
             <template slot-scope="scope">
-              <div class="scope-title_text">
-                {{scope.row.name|| '-'}}
+              <div class="scope-title">
+                <div class="scope-title_text">
+                  {{scope.row.name||'-'}}
+                </div>
               </div>
             </template>
           </el-table-column>
-          <el-table-column
-            prop="guideName"
-            label="所属员工">
+          <el-table-column width="120px" prop="guideName" label="所属员工">
             <template slot-scope="scope">
               <div class="scope-title_text">
                 {{scope.row.guideName|| '-'}}
               </div>
             </template>
           </el-table-column>
-          <el-table-column
-            prop="recruitment"
-            label="员工所属门店">
+          <el-table-column prop="guideName" label="员工所属门店">
             <template slot-scope="scope">
-              <div class="scope-title_text">
-                {{scope.row.recruitment|| '-'}}
+              <div class="scope-name">
+                <div :class="'scope-name_text'" >
+                  {{scope.row.offlineShopsStr || '-'}}
+                </div>
+                <el-popover v-if="scope.row.offlineShopsStr"
+                            placement="top-start"
+                            class="item"
+                            width="180"
+                            trigger="hover"
+                            :content="scope.row.offlineShopsStr||''">
+                  <span class="scope-name_tip" slot="reference">共{{scope.row.offlineShopsStr.split(',').length}}个</span>
+                </el-popover>
               </div>
             </template>
           </el-table-column>
-          <el-table-column
-            prop="prizeType"
-            label="奖励类型">
+          <el-table-column prop="prizeType" label="奖励类型">
             <template slot-scope="scope">
               <div class="scope-title_text">
-                {{scope.row.prizeType == 1 ? '优惠券': '-'}}
+                {{prizeTypeList[scope.row.prizeType].value}}
               </div>
             </template>
           </el-table-column>
-          <el-table-column
-            prop="prizeName"
-            label="奖励内容">
+          <el-table-column prop="prizeGrade" label="奖励阶梯">
+            <template slot-scope="scope">
+              <div class="scope-title_text">
+                {{prizeGradeList[scope.row.prizeGrade].value}}
+              </div>
+            </template>
+          </el-table-column>
+          <el-table-column prop="prizeName" label="奖励内容">
             <template slot-scope="scope">
               <div class="scope-title_text">
                 {{scope.row.prizeName || '-'}}
               </div>
             </template>
           </el-table-column>
-          <el-table-column
-            prop="time"
-            label="领取时间">
+          <el-table-column prop="receiveTime" label="领取时间">
             <template slot-scope="scope">
               <div class="scope-title_text">
-                {{scope.row.time || '-'}}
+                {{scope.row.receiveTime|| '-'}}
               </div>
             </template>
           </el-table-column>
@@ -115,60 +141,86 @@
 import PageTable from '../PageTable'
 import tableMixin from '@nascent/ecrp-ecrm/src/mixins/table'
 import NsGuideDialog from '@/components/NsGuideDialog'
+import defaultIcon from '@/assets/defultheadPic.png'
 import moment from 'moment'
-import { getErrorMsg } from '@/utils/toast'
 export default {
   data () {
     return {
       model: {
         timeStart: null,
         timeEnd: null,
-        name: null,
+        employeeName: null,
         guideIds: null,
+        guestCodeStatus: null,
         guestCodeId: this.$route.query.guestCodeId,
-        presentsState: null // 赠送状态
+        inviteFriendNo: null,
+        lastAddFriendsDate: null
       },
-      exportState: true,
       validTimeStart: this.$route.query.validTimeStart,
-      url: this.$api.guide.customerCode.findActivityReachStandard,
+      url: this.$api.guide.customerCode.findAwardRecordTable,
       seachVal: '',
+      prizeName: '',
+      seachDate: [],
       guideIds: [],
-      activeIndex: -1,
+      exportState: true,
       statusList: [
         {
           label: '全部',
           value: null
         }, {
-          label: '发放失败',
-          value: 0
-        }, {
-          label: '已领取',
+          label: '优惠券',
           value: 1
         }, {
-          label: '待领取',
+          label: '红包',
           value: 2
         }
-      ]
-      // data: { 'success': true, 'result': { 'draw': 0, 'data': [{ 'guideId': '1007162', 'employeeName': '莫志敏', 'employeeNumber': '18458445553', 'employeeMobile': '18458445550', 'offlineShops': ['莫志敏测试店铺', '张邵成企微测试线下门店01', '周杰伦的线下门店', '张邵成企微测试线下门店02', '杭州企微_dzm_001'], 'promotionMasterNumber': 5, 'inviteFriendNumber': 6 }, { 'guideId': '1010408', 'employeeName': '夏敏洁测试2', 'employeeNumber': '18551345765', 'offlineShops': ['夏敏洁测试门店01', '夏敏洁测试门店02', '夏敏洁测试门店03', '夏敏洁企微线下门店05', 'lytest-1', '线下_xmj_001', '杭州企微_dzm_001'], 'promotionMasterNumber': 0, 'inviteFriendNumber': 0 }, { 'guideId': '1006965', 'employeeName': '张珏', 'employeeNumber': '张珏', 'employeeMobile': '13709382014', 'offlineShops': ['周杰伦的线下门店', '线下门店b'], 'promotionMasterNumber': 0, 'inviteFriendNumber': 0 }, { 'guideId': '1006964', 'employeeName': '许志龙', 'employeeNumber': 'Aa123456', 'employeeMobile': '18559025686', 'offlineShops': ['昆凌的线下门店', '线下门店a'], 'promotionMasterNumber': 0, 'inviteFriendNumber': 0 }, { 'guideId': '1006962', 'employeeName': '谢燕珊', 'employeeNumber': '谢燕珊', 'employeeMobile': '15980756362', 'offlineShops': ['线下-wha01', 'xys企微线下店'], 'promotionMasterNumber': 0, 'inviteFriendNumber': 0 }, { 'guideId': '1006954', 'employeeName': '梁秋霞', 'employeeNumber': '梁秋霞', 'employeeMobile': '15859264967', 'offlineShops': ['鼓浪屿线下店', '会展中心线下店', 'lqx线下店01', 'lqx线下店02', '线下-wha01', '韩金鹏企微新门店门店01', 'lqx集团线下店01', '线下门店-星期三003', '线下门店-星期三004', '线下_xmj_001', '企微_xmj_01'], 'promotionMasterNumber': 0, 'inviteFriendNumber': 0 }, { 'guideId': '1006948', 'employeeName': '申志奎', 'employeeNumber': '申志奎', 'employeeMobile': '17681806002', 'offlineShops': ['线下门店a'], 'promotionMasterNumber': 0, 'inviteFriendNumber': 0 }, { 'guideId': '1006947', 'employeeName': '郑兴强', 'employeeNumber': '郑兴强', 'employeeMobile': '15960253866', 'offlineShops': ['线下门店a'], 'promotionMasterNumber': 0, 'inviteFriendNumber': 0 }, { 'guideId': '1006942', 'employeeName': '黄宇业', 'employeeNumber': 'test20200403', 'employeeMobile': '13185378611', 'offlineShops': ['周杰伦的线下门店', '昆凌的线下门店', '线下门店a', '联想软件园店', '线下门店b', 'lytest-1', 'lytest-2', '夏敏洁测试门店04', '夏敏洁企微线下门店05'], 'promotionMasterNumber': 0, 'inviteFriendNumber': 0 }, { 'guideId': '1006941', 'employeeName': '徐磊', 'employeeNumber': '13733105819', 'employeeMobile': '13733105819', 'offlineShops': ['于亚洲的线下门店01', '周杰伦的线下门店', '昆凌的线下门店', '线下门店a', '线下门店b', '线下门店c', '线下门店d', '夏敏洁企微线下门店05'], 'promotionMasterNumber': 0, 'inviteFriendNumber': 0 }, { 'guideId': '1006939', 'employeeName': '测试xmj', 'employeeNumber': '13345678001', 'employeeMobile': '13345678001', 'offlineShops': ['数字门店AB线下', '夏敏洁测试门店01', '夏敏洁测试门店02', '夏敏洁测试门店03', '夏敏洁测试门店04', '夏敏洁企微线下门店05'], 'promotionMasterNumber': 0, 'inviteFriendNumber': 0 }, { 'guideId': '1006932', 'employeeName': '刘影', 'employeeNumber': 'ly0804', 'employeeMobile': '18355302093', 'offlineShops': ['南讯企业微信店铺', 'lytest-1', 'lytest-2', 'lytest0427', 'lytest-0521', '数字门店AB线下', '周杰伦的线下门店', '昆凌的线下门店', '线下门店a', '线下门店b', '线下门店c', '线下门店d', '联想观音山店', '联想软件园店', '美的空调店', '美的微波炉店', '线下永安1店', '线下永安2店', '线下永安3店', '线下大湖1店', '线下大湖2店', '线下门店-HJP-积分兑换001'], 'promotionMasterNumber': 0, 'inviteFriendNumber': 0 }, { 'guideId': '1006931', 'employeeName': '彭云超', 'employeeNumber': '彭云超', 'employeeMobile': '18132097151', 'offlineShops': ['导购招募测试门店', '永安测导购招募', 'lytest-2', 'lytest-1', '韩金鹏企微线下门店', '周杰伦的线下门店', '昆凌的线下门店', '美的小家电', '线下门店b', '线下门店c', '线下门店d', 'cyf-线下-01', '线下门店a', '新增门店', '线下门店', '邓志明的企微门店', '暂停营业数字门店的线下门店'], 'promotionMasterNumber': 0, 'inviteFriendNumber': 0 }, { 'guideId': '1006927', 'employeeName': '于亚洲', 'employeeNumber': '18551770017', 'employeeMobile': '18551770017', 'offlineShops': ['会展中心线下店', '于亚洲的线下门店01', '于亚洲的测试视角01', '于亚洲的测试视角008', '夏敏洁测试门店01', '周杰伦的线下门店', '昆凌的线下门店', '线下门店a', '线下门店b', '线下门店c', '线下门店d', '联想观音山店', '联想软件园店', '美的空调店', '美的微波炉店', '线下永安1店', '线下永安2店', '线下永安3店', '线下大湖1店', '线下大湖2店'], 'promotionMasterNumber': 0, 'inviteFriendNumber': 0 }, { 'guideId': '1005761', 'employeeName': '张泽川', 'employeeNumber': '张泽川', 'employeeMobile': '15868874895', 'offlineShops': ['南讯企业微信店铺'], 'promotionMasterNumber': 0, 'inviteFriendNumber': 0 }], 'recordsTotal': '17', 'recordsFiltered': '17' } }
+      ],
+      // 奖励类型
+      prizeTypeList: {
+        0: {
+          value: '未知'
+        },
+        1: {
+          value: '优惠券'
+        },
+        2: {
+          value: '红包'
+        }
+      },
+      // 奖励阶梯
+      prizeGradeList: {
+        0: {
+          value: '未知'
+        },
+        1: {
+          value: '阶梯一'
+        },
+        2: {
+          value: '阶梯二'
+        },
+        3: {
+          value: '阶梯三'
+        },
+        4: {
+          value: '阶梯四'
+        },
+        5: {
+          value: '阶梯五'
+        }
+      },
+      activeIndex: -1,
+      defaultIcon
     }
   },
   components: { PageTable, NsGuideDialog },
-  props: ['startTime', 'endTime'],
   mixins: [tableMixin],
+  props: ['startTime', 'endTime'],
   mounted () {
     this.$searchAction$()
   },
   methods: {
-    // 修改请求参数
-    changeSearchfrom (obj = {}) {
-      this.model = Object.assign(this.model, obj)
-      this.$searchAction$()
-    },
     handleChangeGuide (value) {
       this.changeSearchfrom({ guideIds: value.length > 0 ? value.join(',') : null })
-    },
-    handleSearch () {
-      this.changeSearchfrom({ name: this.seachVal })
     },
     exportClick () {
       if (!this._data._table.data.length) {
@@ -188,7 +240,7 @@ export default {
       let that = this
       that.$notify.info('导出中，请稍后片刻')
       this.$http
-        .fetch(this.$api.guide.customerCode.reachStandardExport, params)
+        .fetch(this.$api.guide.customerCode.awardRecordTableExport, params)
         .then(resp => {
           that.exportState = true
           that.$notify.success('下载完成')
@@ -211,30 +263,85 @@ export default {
             } else {
               time = '全部'
             }
-            let fileName = '达标总人数明细' + time + '.csv'
+            let fileName = '领奖记录明细' + time + '.csv'
             link.setAttribute('download', fileName)
             document.body.appendChild(link)
             link.click()
           }
         })
     },
-    // 排序
+    handleSearch () {
+      this.changeSearchfrom({ name: this.seachVal, prizeName: this.prizeName })
+    },
+    // 修改请求参数
+    changeSearchfrom (obj = {}) {
+      this.model = Object.assign(this.model, obj)
+      this.$searchAction$()
+    },
     handleSort (data) {
       const order = data.order
       const prop = data.prop
       const sortData = {
-        promotionMasterNumber: 'masterOrder',
-        inviteFriendNumber: 'friendOrder',
-        descending: 'desc',
-        ascending: 'asc'
+        inviteFriendNo: 'inviteFriendNo',
+        lastAddFriendsDate: 'addTimeOrderNo',
+        descending: 0,
+        ascending: 1
       }
-      this.changeSearchfrom(Object.assign({}, { masterOrder: null, friendOrder: null }, { [sortData[prop]]: sortData[order] }))
+      this.changeSearchfrom(Object.assign({}, { inviteFriendNo: null, addTimeOrderNo: null }, { [sortData[prop]]: sortData[order] }))
+    },
+    getOhterGuideForFriend (type) {
+      this.getOhterGuide(type, this.handleShowFriend)
+    },
+    // 查看上一个或下一个导购或好友
+    getOhterGuide (type, cb) {
+      const { page, size, total } = this._data._pagination
+      if (type === 'prev') {
+        if (this.activeIndex === 0) {
+          if (page === 1) {
+            this.$notify.error('暂无上一个裂变大师')
+          } else {
+            this._data._pagination.page = page - 1
+            this.$queryList$(this.$generateParams$()).then(() => {
+              const index = this._data._table.data.length - 1
+              cb(this._data._table.data[index], index)
+            })
+          }
+        } else {
+          const index = this.activeIndex - 1
+          cb(this._data._table.data[index], index)
+        }
+      } else if (type === 'next') {
+        if (((page - 1) * size + this.activeIndex + 1) >= total) {
+          this.$notify.error('暂无下一个裂变大师')
+        } else {
+          if (this.activeIndex === size - 1) {
+            this._data._pagination.page = page + 1
+            this.$queryList$(this.$generateParams$()).then(() => {
+              const index = 0
+              cb(this._data._table.data[index], index)
+            })
+          } else {
+            const index = this.activeIndex + 1
+            cb(this._data._table.data[index], index)
+          }
+        }
+      }
+    },
+    tableRowClassName ({ row, rowIndex }) {
+      if (rowIndex === this.activeIndex) {
+        return { backgroundColor: '#D9EFFE' }
+      }
+      return ''
     },
     clearActiveIndex () {
       this.activeIndex = -1
     }
   },
   watch: {
+    seachDate (newVal) {
+      const date = newVal || [null, null]
+      this.changeSearchfrom({ receiveTimeStart: date[0], receiveTimeEnd: date[1] })
+    },
     startTime: {
       handler (newVal) {
         this.changeSearchfrom({ timeStart: newVal, timeEnd: this.endTime })
@@ -246,10 +353,6 @@ export default {
 </script>
 <style lang="scss" scoped>
 @import "../../styles/reset.css";
-.search-icon {
-  font-size: 22px;
-  margin-top: 2px;
-}
 .scope-name_text {
   overflow: hidden;
   text-overflow: ellipsis;
@@ -263,6 +366,37 @@ export default {
 }
 .max-popper {
   max-width: 450px;
+}
+.scope-title {
+  display: flex;
+  align-items: center;
+}
+.scope-title_img {
+  height: 32px;
+  width: 32px;
+  margin-right: 8px;
+}
+.scope-name_tag.el-tag {
+  border-radius: 2px;
+  &.el-tag--success {
+    background: #F7FFF0;
+    border: 1px solid #53BF1D;
+    color: #262626;
+  }
+  &.el-tag--info {
+    background: #F5F5F5;
+    border: 1px solid #D9D9D9;
+    color: #262626;
+  }
+  &.el-tag--warning {
+    background: #FFFBE6;
+    border: 1px solid #FFAE0D;
+    color: #262626;
+  }
+}
+.search-icon {
+  font-size: 22px;
+  margin-top: 2px;
 }
 .self-btn {
   width: 150px;
