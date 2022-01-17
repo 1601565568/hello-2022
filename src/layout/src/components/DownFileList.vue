5f80a42c572197e954335e06331d42a43d22596b
haixin <1367228921@qq.com>
2022-01-17 10:18:30 +0800

    添加搜索条件

--- src/layout/src/components/DownFileList.vue
+++ src/layout/src/components/DownFileList.vue
@@ -16,44 +16,77 @@
       <div class="remind-view">
         下载中心仅保留近3天生成的报表，请尽快下载
       </div>
-      <div class="down-search"></div>
+      <div class="down-search">
+        <div class="input-view">
+          <el-input
+            placeholder="请输入文件名称"
+            autofocus="false"
+            type="text"
+            v-model="inputTitle"
+            @change="inputChange"
+          >
+            <Icon
+              type="ns-search"
+              slot="suffix"
+              style="font-size: 30px;"
+              @click="inputChange"
+            ></Icon>
+          </el-input>
+        </div>
+        <div class="input-view">
+          <el-input
+            placeholder="请输入生成时间"
+            autofocus="false"
+            type="text"
+            v-model="inputTitle"
+            @change="inputChange"
+          >
+            <Icon
+              type="ns-search"
+              slot="suffix"
+              style="font-size: 30px;"
+              @click="inputChange"
+            ></Icon>
+          </el-input>
+        </div>
+      </div>
       <div class="down-table" v-if="listData.length > 0">
         <page-table style="padding-top:0">
-            <template slot="table">
-              <el-table
-                :data="listData"
-                class="new-table_border drawer-table"
-                :row-style="{ height: '48px' }"
-              >
-                <el-table-column prop="guideName" label="文件名称">
-                </el-table-column>
-                <el-table-column prop="phone" label="生成时间">
-                  <template slot-scope="scope">{{
-                    scope.row.phone || '-'
-                  }}</template>
-                </el-table-column>
-                <el-table-column prop="post" label="操作">
-                  <template slot-scope="scope">{{
-                    transPost(scope.row.post)
-                  }}</template>
-                </el-table-column>
-              </el-table>
-            </template>
-            <template slot="pagination">
-              <el-pagination
-                background
-                class="label-dialog__pagination"
-                :page-sizes="paginationToPerson.sizeOpts"
-                :total="paginationToPerson.total"
-                :current-page.sync="paginationToPerson.page"
-                :page-size="paginationToPerson.size"
-                layout="total, sizes, prev, pager, next, jumper"
-                @size-change="handleSizeChangeForPerson"
-                @current-change="handleCurrentChangeForPerson"
-              >
-              </el-pagination>
-            </template>
-          </page-table>
+          <template slot="table">
+            <el-table
+              :data="listData"
+              class="new-table_border drawer-table"
+              :row-style="{ height: '48px' }"
+            >
+              <el-table-column prop="guideName" label="文件名称">
+              </el-table-column>
+              <el-table-column prop="phone" label="生成时间">
+                <template slot-scope="scope">{{
+                  scope.row.phone || '-'
+                }}</template>
+              </el-table-column>
+              <el-table-column prop="post" label="操作">
+                <template slot-scope="scope">{{
+                  transPost(scope.row.post)
+                }}</template>
+              </el-table-column>
+            </el-table>
+          </template>
+          <template slot="pagination">
+            <el-pagination
+              background
+              class="label-dialog__pagination"
+              :page-sizes="paginationToPerson.sizeOpts"
+              :total="paginationToPerson.total"
+              :current-page.sync="paginationToPerson.page"
+              :page-size="paginationToPerson.size"
+              layout="total, sizes, prev, pager, next, jumper"
+              @size-change="handleSizeChangeForPerson"
+              @current-change="handleCurrentChangeForPerson"
+            >
+            </el-pagination>
+          </template>
+        </page-table>
       </div>
       <div v-else>
         <NoData />
@@ -82,7 +115,8 @@ export default {
         page: 1,
         total: 0
       },
-      listData: []
+      listData: [],
+      inputTitle: ''
     }
   },
   methods: {
@@ -104,14 +138,15 @@ export default {
       this.paginationToPerson.page = page
       this.loadDetail()
     },
-    loadDetail () {
-    }
+    loadDetail () {},
+    inputChange () {}
   }
 }
 </script>
 
 <style lang="scss" scoped>
 @import '@components/NewUi/styles/reset.css';
+@import '@pages/WeWork/MaterialChat/styles/index.css';
 .down-view {
   .close-view {
     height: 49px;
@@ -139,12 +174,22 @@ export default {
     color: #595959;
     line-height: 22px;
     padding: 9px 16px;
-    margin: 16px;
+    margin: 16px 16px 0 16px;
   }
 }
 .drawer-table {
   padding: 0;
   font-size: 14px;
   font-weight: 400;
+}
+.down-search {
+  height: 65px;
+  display: flex;
+  flex-direction: row;
+  padding-left: 16px;
+  align-items: center;
+}
+.input-view {
+  margin-right: 16px;
 }
 </style>
