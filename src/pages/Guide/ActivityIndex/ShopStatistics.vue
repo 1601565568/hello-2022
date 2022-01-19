ff24e17cd3b1165eb3c6833c0071fd1bca5fb74f
haixin <1367228921@qq.com>
2022-01-19 13:38:02 +0800

    修改文案

--- src/pages/Guide/ActivityIndex/ShopStatistics.vue
+++ src/pages/Guide/ActivityIndex/ShopStatistics.vue
@@ -691,9 +691,9 @@ export default {
     commissionTypeText (type) {
       let typeText = '-'
       if (type === 0) {
-        typeText = '成单导购提成'
-      } else if (type === 1) {
         typeText = '专属导购提成'
+      } else if (type === 1) {
+        typeText = '成单导购提成'
       }
       return typeText
     },
--- src/pages/Guide/ActivityIndex/WorkStatistics.vue
+++ src/pages/Guide/ActivityIndex/WorkStatistics.vue
@@ -649,9 +649,9 @@ export default {
     commissionTypeText (type) {
       let typeText = '-'
       if (type === 0) {
-        typeText = '成单导购提成'
-      } else if (type === 1) {
         typeText = '专属导购提成'
+      } else if (type === 1) {
+        typeText = '成单导购提成'
       }
       return typeText
     },
