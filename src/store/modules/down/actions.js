d4c7562dead093a4399854f4d621c6fb0ab23c3b
haixin <1367228921@qq.com>
2022-01-20 14:51:01 +0800

    删除日志

--- src/store/modules/down/actions.js
+++ src/store/modules/down/actions.js
@@ -1,6 +1,5 @@
 export default {
   downAction ({ commit }, payload) {
-    console.log('downAction01', payload)
     commit('setDownAction', payload.status)
     commit('setTop', payload.top)
     commit('setRight', payload.right)
