7b3cb4fcdcdb7662b043c2821de1d57810f78c4e
haixin <1367228921@qq.com>
2022-02-10 10:03:46 +0800

    员工组件添加状态

--- src/components/NsGuideDialog/src/index.js
+++ src/components/NsGuideDialog/src/index.js
@@ -121,7 +121,9 @@ export default {
         job: null,
         mobile: '',
         fileImportKey: '', // 文件导入
-        manualInputKey: '' // 手动输入导入
+        manualInputKey: '', // 手动输入导入
+        // 1启用 2离职 3全部
+        status: 3
       },
       // 门店区域树
       shopAreaTree: [],
@@ -178,6 +180,7 @@ export default {
         this.departData.name = ''
         this.departData.mobile = ''
         this.departData.job = null
+        this.departData.status = 3
         this.departData.selectedDepart = {}
         if (this.echoStore) {
           let areaData = store.get('user_area')
@@ -301,6 +304,7 @@ export default {
       vm.departData.name = ''
       vm.departData.mobile = ''
       vm.departData.job = ''
+      vm.departData.status = 3
       vm.departData.selectedDepart = {}
       vm.departData.shopArea = {} // 选择的门店分类
       vm.departData.shopId = '' // 选择的门店
@@ -388,6 +392,9 @@ export default {
       }
       if (vm.departData.job !== null) {
         param.job = vm.departData.job
+      }
+      if (vm.departData.status !== null) {
+        param.status = vm.departData.status
       }
       if (vm.departData.mobile) {
         param.mobile = vm.departData.mobile
