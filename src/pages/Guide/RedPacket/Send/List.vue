5887e91fc28f428423876faf9096f43490f5b88b
lqh <qihang.li@nascent.cn>
2022-02-24 21:14:10 +0800

    fix 测试发布232

--- src/pages/Guide/RedPacket/Send/List.vue
+++ src/pages/Guide/RedPacket/Send/List.vue
@@ -1,5 +1,5 @@
 <template>
-    <iframe id='linkedFrame' hight='auto' width='100%' :src='iframeUrl' frameborder='0' scrolling='auto' onload='this.height=1000'></iframe>
+    <iframe id='linkedFrame' hight='auto' width='100%' src='https://sandbox-sg-v6.vecrp.com/v3/friendsDrainage/demo' frameborder='0' scrolling='auto' onload='this.height=1000'></iframe>
 </template>
 <script>
 import Index from './src/list'
