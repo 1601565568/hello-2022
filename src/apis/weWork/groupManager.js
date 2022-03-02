58d9a46bbb3c56d5b6afae2c651387a59f96c687
张平 <ping.zhang@nascent.cn>
2022-03-02 11:44:31 +0800

    fix:删除多余接口

--- src/apis/weWork/groupManager.js
+++ src/apis/weWork/groupManager.js
@@ -4,37 +4,9 @@ export default {
     url: '/wework/weworkRooms/name',
     method: 'post'
   },
-  getGroupMemberList: {
-    url: '/userCenter/groupManager/queryGroupMemberList',
-    method: 'post'
-  },
-  queryGroupListByOwnerId: {
-    url: '/userCenter/groupManager/queryGroupListByOwnerId',
-    method: 'post'
-  },
   // 选择群聊列表
   getGroupNameList: {
     url: '/chatRoom/canJoinList',
-    method: 'post'
-  },
-  findGroupNameTable: {
-    url: '/userCenter/groupManager/findGroupNameTable',
-    method: 'post'
-  },
-  getRepeatMemberListByChatId: {
-    url: '/userCenter/groupManager/queryRepeatMemberListByChatId',
-    method: 'post'
-  },
-  getGroupAnalysisByDate: {
-    url: '/userCenter/groupManager/queryGroupAnalysisByDate',
-    method: 'post'
-  },
-  getGroupAnalysisByUser: {
-    url: '/userCenter/groupManager/queryGroupAnalysisByUser',
-    method: 'post'
-  },
-  getGroupAnalysisList: {
-    url: '/userCenter/groupManager/getGroupAnalysisList',
     method: 'post'
   },
   // 群主下拉列表
@@ -42,20 +14,12 @@ export default {
     url: '/wework/weworkRooms/owner',
     method: 'post'
   },
-  findGroupMemberDetail: {
-    url: '/userCenter/customerList/findGroupMemberDetail',
-    method: 'post'
-  },
-  queryGroupAnalysisOverview: {
-    url: '/userCenter/groupManager/queryGroupAnalysisOverview',
-    method: 'post'
-  },
-  // 所有用户
+  // 所有群聊成员
   queryRepeatedInContactList: {
     url: '/wework/externalContact/queryRepeatedInContactList',
     method: 'post'
   },
-  // 详情
+  // 群聊成员详情
   queryRepeatedInContactDetailList: {
     url: '/wework/weworkRoomMember/queryRepeatedInContactDetailList',
     method: 'post'
