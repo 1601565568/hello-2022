fca1d7ae38391e1ca0f78f6a519e33661fd87276
mozhimin <mozhimin1993@163.com>
2022-03-04 19:57:58 +0800

    fix(群列表): 工号显示回滚

--- src/pages/Guide/RedPacket/Analysis/components/TableByGuide.vue
+++ src/pages/Guide/RedPacket/Analysis/components/TableByGuide.vue
@@ -13,7 +13,7 @@
           prop="wokerNumber"
           label="工号">
           <template slot-scope="scope">
-            {{scope.row.workNumber || '-'}}
+            {{scope.row.wokerNumber || '-'}}
           </template>
         </el-table-column>
         <el-table-column
