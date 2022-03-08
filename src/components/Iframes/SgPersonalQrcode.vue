d90704c7b146e93390e640c8231a496dc26625ea
lqh <qihang.li@nascent.cn>
2022-03-08 15:20:05 +0800

    fix 更改右侧滚跳样式

--- src/components/Iframes/SgPersonalQrcode.vue
+++ src/components/Iframes/SgPersonalQrcode.vue
@@ -1,3 +1,3 @@
 <template>
-    <iframe id='linkedFrame' width='100%' src='https://sandbox-sg-v6.vecrp.com/v3/friendsDrainage/aggregationCodeList/' frameborder='0' scrolling='auto' style="height: calc(100vh - 100px);margin-left: -10px;margin-top: -10px;"></iframe>
+    <iframe id='linkedFrame' width='100%' src='https://sandbox-sg-v6.vecrp.com/v3/friendsDrainage/aggregationCodeList/' frameborder='0' scrolling='auto' style="height: calc(100vh - 110px);margin-left: -10px;margin-top: -10px;"></iframe>
 </template>
