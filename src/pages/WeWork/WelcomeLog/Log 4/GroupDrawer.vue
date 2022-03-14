42dea43df75d704969a2b6ca532811fbe73579f2
彭强 <18758866021@163.com>
2022-03-14 10:17:48 +0800

    fix：代码合并

---
+++ src/pages/WeWork/WelcomeLog/Log 4/GroupDrawer.vue
@@ -0,0 +1,63 @@
+<template>
+  <ElDrawer
+    size="720px"
+    class="group-drawer"
+    :modal="false"
+    :visible="visible"
+    direction="rtl"
+    @close="$emit('update:visible', false)"
+  >
+    <template slot="title">
+      <h3 class="title">发送群</h3>
+    </template>
+    <ActivityGroup
+      type="log"
+      :reload="visible"
+      :activityId="activityId"
+    />
+  </ElDrawer>
+</template>
+
+<script>
+import ElDrawer from '@nascent/nui/lib/drawer'
+import ActivityGroup from '../components/ActivityGroup/index.vue'
+
+export default {
+  components: {
+    ElDrawer,
+    ActivityGroup
+  },
+  props: {
+    visible: {
+      type: Boolean,
+      default: false
+    },
+    activityId: Number
+  },
+  mounted () {
+  },
+  methods: {}
+}
+</script>
+
+<style lang="scss" scoped>
+@import "./styles/drawer-reset.css";
+
+.group-drawer {
+  .title {
+    font-size: 16px;
+    color: #262626;
+    line-height: 24px;
+  }
+
+  ::-webkit-scrollbar {
+    width: 6px;
+    height: 6px;
+  }
+
+  ::-webkit-scrollbar-thumb {
+    border-radius: 10px;
+    background: #9093994d;
+  }
+}
+</style>
