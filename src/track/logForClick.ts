8c6f58dfed3f24813b7c82ff0def33cd068384b9
海信 <1367228921@qq.com>
2022-03-23 14:28:32 +0800

    修改埋点参数

--- src/track/logForClick.ts
+++ src/track/logForClick.ts
@@ -1,18 +1,20 @@
 import WhatsElement from 'whats-element/pure'
 import router from '../router'
+
 const whats = new WhatsElement()
 let pathuuidMapList:any[] = []
+let defaultParams = {}
 
 type EventMapOption = {
-  includes?:String[] // 仅包含的标签
-  excludes?:String[] // 不包含的标签
+  includes?:string[] // 仅包含的标签
+  excludes?:string[] // 不包含的标签
 }
 type EventMap = {
   [key:string]: EventMapOption
 }
 
 /**
- * vue页面事件监听 （不支持阻止冒泡事件）
+ * 页面事件监听 （不支持阻止冒泡事件）
  * @param {EventMap} eventMap 事件配置
  * @param {string} timer 防抖时间
  */
@@ -24,9 +26,9 @@ export const eventProxy = (eventMap:EventMap = { click: {} }, timer:number) => {
       clearTimeout(t)
       t = setTimeout(() => {
         if (event.path && event.path.length) {
+          // 判断冒泡过程中是否有定义需要监听的事件
           const clickItem = event.path.find((item:any) => {
-            // 判断冒泡过程中是否有定义需要监听的事件
-            const vueEvent = getVueEvent(item)
+            const vueEvent = getVueEvent(item) // vue的event对象
             if (vueEvent) {
               return Object.keys(vueEvent).some(itmes => {
                 const eventOption = eventMap[itmes]
@@ -40,7 +42,7 @@ export const eventProxy = (eventMap:EventMap = { click: {} }, timer:number) => {
           })
           if (clickItem) {
             const parmas = formatParmas(getParmasUuid(clickItem))
-            window.logger.send(parmas)
+            window.logger.send({ ...parmas, ...defaultParams })
           }
         }
       }, timer)
@@ -51,14 +53,14 @@ export const eventProxy = (eventMap:EventMap = { click: {} }, timer:number) => {
   } catch (e) {
   }
 }
-
-/**
- * 获取vue event对象
- */
-const getVueEvent = (item:any) => item.__vue__ && item.__vue__._events ? item.__vue__._events : null
+export const setDefaultParmas = (parmas:{[key:string]:any}) => {
+  defaultParams = Object.assign(defaultParams, parmas)
+}
 
 /**
  * 处理eventMap配置
+ * @param {*} option
+ * @param {HTMLElement} ele
  */
 const analysisOption = (option:any, ele:HTMLElement):boolean => {
   // 不包含的标签
@@ -99,7 +101,7 @@ const getParmasUuid = (ele:HTMLElement) => {
 
 /**
  * 获取元素在dom中的路径
- * @param {String} uuid
+ * @param {string} uuid
  */
 const formatParmas = (uuid:string) => ({ type: 'event', uuid })
 
@@ -114,6 +116,54 @@ const getUUID = () => {
     return (c === 'x' ? r : (r & 0x3 | 0x8)).toString(16)
   })
   return uuid
+}
+
+/**
+ * log里组装的数据和想要的不同，暂时通过重写替换window.logger.send方法
+ *
+ */
+const send = function (this:any, params: { spm: any; type: any }) {
+  if (params && !this.config.disabled) {
+    var spm = this.getSpmId() // 如果尚未设置过spm,并且成功获取到spm,则锁定spm值
+    // 加入队列的时候即固定了此次上报的spm,方便SPA应用通过setConfig动态修改spm
+
+    if (!params.spm && spm) {
+      params.spm = spm
+    }
+    const type = params.type
+    this.queue({
+      // appType: appEnv.getWebDev(),
+      // appEnv: appEnv.getAppEnv(),
+      nodeEnv: process.env.NODE_ENV,
+      timestamp: new Date().getTime(),
+      url: window.location.origin + window.location.pathname,
+      path: getVueRouter(),
+      ...defaultParams,
+      ...params,
+      type
+    })
+  }
+}
+setTimeout(() => {
+  window.logger.send = send.bind(window.logger)
+}, 0)
+
+/**
+ * 获取vue event对象
+ * @param {*} item
+ */
+const getVueEvent = (item:any) => item.__vue__ && item.__vue__._events ? item.__vue__._events : null
+
+/**
+ * 获取vue router
+ * @param {*} item
+ */
+const getVueRouter = () => {
+  try {
+    return router.history.current.path
+  } catch (e) {
+    return window.location.pathname
+  }
 }
 
 router.beforeEach((to, from, next) => {
